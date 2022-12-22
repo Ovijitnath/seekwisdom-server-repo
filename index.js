0ad43bc8dea7f8ce2953831a3eaaf92d578b6161
Ovijitnath <ovijitnathgn@gmail.com>
2022-12-22 20:29:40 +0600

    comments included

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+
+// Loading Data
 const catagories = require('./data/catagories.json');
 
 const courses = require('./data/courses.json');
@@ -14,22 +16,29 @@ app.get('/', (req, res) => {
     res.send('News API Running');
 });
 
+// Loading catagories
 app.get('/catagories', (req, res) => {
     res.send(catagories);
 });
 
+
+// Loading catagories by ID
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourses = courses.find(c => c._id === id);
 
     res.send(selectedCourses);
 });
+
+
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     const CourseDetails = courses.filter(c => c.category_id == id);
 
     res.send(CourseDetails);
 });
+
+// Checkout page as per user login
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
     const CourseDetails = courses.filter(c => c.category_id == id);
